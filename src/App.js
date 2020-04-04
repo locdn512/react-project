dbdc912486e2b870ae7846774788cd206c1177ad
locdn512 <43569893+locdn512@users.noreply.github.com>
2020-04-05 01:59:14 +0700

    Lộc

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './containers/HomePage';
+import Header from './components/Header';
+import Benefit from './components/Benefit';
+import Footer from './components/Footer';
 
 
 
 function App() {
   // const name = "Đặng Nguyên Lộc";
-  
+
   return (
-    
+
     <div className="App">
+      <Header />
       <HomePage />
+      <Benefit />
+      <Footer />
     </div>
   );
 }
